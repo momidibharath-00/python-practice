d3a6e5390f3dd9ce66e6a65840326c93a3b4fc3a
Momidi Bharath <momidibharath34@gmail.com>
2026-07-01 20:48:16 +0530

    Add string_functions.py

---
+++ Chapter-03-Strings/string_functions.py
@@ -0,0 +1,8 @@
+name = input("Enter a string: ")
+
+print("Original String :", name)
+print("Upper Case      :", name.upper())
+print("Lower Case      :", name.lower())
+print("Title Case      :", name.title())
+print("Capitalized     :", name.capitalize())
+print("Length          :", len(name))
