45a464268395facdc3736ee9fbd7a9300f3dce3e
Momidi Bharath <momidibharath34@gmail.com>
2026-07-01 20:47:02 +0530

    Add string_slicing.py

---
+++ Chapter-03-Strings/string_slicing.py
@@ -0,0 +1,6 @@
+name = "Programming"
+
+print("Original String      :", name)
+print("First 5 Characters   :", name[:5])
+print("Last 4 Characters    :", name[-4:])
+print("Characters 3 to 7    :", name[3:8])
