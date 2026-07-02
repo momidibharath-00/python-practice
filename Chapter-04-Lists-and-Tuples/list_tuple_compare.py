26ea2567357d8033b386dc4ebc815827a492b57a
Momidi Bharath <momidibharath34@gmail.com>
2026-07-02 22:55:39 +0530

    Add list_tuple_compare.py

---
+++ Chapter-04-Lists-and-Tuples/list_tuple_compare.py
@@ -0,0 +1,7 @@
+fruits_list = ["Apple", "Banana", "Mango"]
+fruits_tuple = ("Apple", "Banana", "Mango")
+print("List:", fruits_list)
+print("Tuple:", fruits_tuple)
+fruits_list.append("Orange")
+print("Updated List:", fruits_list)
+print("Tuples cannot be modified because they are immutable.")
