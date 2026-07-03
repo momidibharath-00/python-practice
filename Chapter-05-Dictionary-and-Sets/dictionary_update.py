ec81132a6ba64ca463ef80655906924cd4b19981
Momidi Bharath <momidibharath34@gmail.com>
2026-07-03 22:07:09 +0530

    Add dictionary_update.py

---
+++ Chapter-05-Dictionary-and-Sets/dictionary_update.py
@@ -0,0 +1,11 @@
+student = {
+    "Name": "Nandu",
+    "Age": 18,
+    "City": "Nellore"
+}
+print("Original Dictionary:")
+print(student)
+student.update({"Age": 19})
+student.update({"Course": "Python"})
+print("Updated Dictionary:")
+print(student)
