c5ab606e948b3dde5a77a9c1e40cb9c5f755f36f
Momidi Bharath <momidibharath34@gmail.com>
2026-07-03 22:36:31 +0530

    Add dictionary_set_compare.py

---
+++ Chapter-05-Dictionary-and-Sets/dictionary_set_compare.py
@@ -0,0 +1,14 @@
+student = {
+    "Name": "Nandu",
+    "Age": 18,
+    "City": "Nellore"
+}
+fruits = {"Apple", "Banana", "Mango"}
+print("Dictionary:")
+print(student)
+print("\nSet:")
+print(fruits)
+print("\nDictionary stores data as key-value pairs.")
+print("Set stores only unique values.")
+print("Dictionary is ordered (Python 3.7+).")
+print("Set is unordered.")
