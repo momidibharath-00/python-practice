6fe7fb7c578fa4470281413235af3568816a7a1e
Momidi Bharath <momidibharath34@gmail.com>
2026-07-03 22:31:05 +0530

    Add set_methods.py

---
+++ Chapter-05-Dictionary-and-Sets/set_methods.py
@@ -0,0 +1,8 @@
+fruits = {"Apple", "Banana", "Mango"}
+print("Original Set:", fruits)
+fruits.add("Orange")
+print("After add():", fruits)
+fruits.remove("Banana")
+print("After remove():", fruits)
+fruits.pop()
+print("After pop():", fruits)
