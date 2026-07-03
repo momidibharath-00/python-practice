bf6af17d05a1d90e9dd90d7218542278c6a55394
Momidi Bharath <momidibharath34@gmail.com>
2026-07-03 22:21:55 +0530

    Add dictionary_access.py

---
+++ Chapter-05-Dictionary-and-Sets/dictionary_access.py
@@ -0,0 +1,10 @@
+student = {
+    "Name": "Nandu",
+    "Age": 18,
+    "City": "Nellore",
+    "Course": "Python"
+}
+print(student["Name"])
+print(student["Age"])
+print(student["City"])
+print(student["Course"])
