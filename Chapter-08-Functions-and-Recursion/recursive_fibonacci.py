4f18a497e94fa5cd8eddd7a0937ee5ede7d37e0a
Momidi Bharath <momidibharath34@gmail.com>
2026-07-10 15:54:28 +0530

    Add recursive_fibonacci.py

---
+++ Chapter-08-Functions-and-Recursion/recursive_fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+number = int(input("Enter a number: "))
+
+print("Fibonacci =", fibonacci(number))
