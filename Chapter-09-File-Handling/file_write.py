4939fdf1d43f2d8b29e18e2af9d58b6720818ca7
Momidi Bharath <momidibharath34@gmail.com>
2026-07-23 22:36:57 +0530

    Add file_write.py

---
+++ Chapter-09-File-Handling/file_write.py
@@ -0,0 +1,12 @@
+# Open the file in write mode
+file = open("output.txt", "w")
+
+# Write text into the file
+file.write("Python File Handling\n")
+file.write("Learning Write Mode")
+
+# Close the file
+file.close()
+
+# Display success message
+print("Data written successfully.")
